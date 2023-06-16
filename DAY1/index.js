f27f4c3e2eb93010b4d1b579a6dc55200c12dafd
Raksha Khadka <rakshakhadka58@gmail.com>
2023-06-16 15:06:08 +0545

    next commit

--- DAY1/index.js
+++ DAY1/index.js
@@ -33,3 +33,38 @@ if((!x)){
     console.log("x is not true");
 }
 
+//Ternary operator
+if(x==1){
+    console.log("x is 1");
+}
+else{
+    console.log("x is not 1");
+}
+
+//if-else ternary op
+let result = x ===1 ? "x is 1" : "x is not 1";
+
+//if-elseif-else ternary op
+result = x === 1 ? "x is 1" : x ===2 ? "x is 2" : "x is not 1 or 2";
+
+//loop
+for(let i=0;i<5;i++){
+    console.log(i);
+}
+
+//ES5 style
+//Functions
+// function add(a, b){
+//     console.log(a+b);
+// }
+function area(a,b){
+    console.log(a*b)
+}
+area(1,2)
+//arrow function (ES6 style)
+const add = (a,b)=>{
+    console.log(a+b);
+}
+add(2,3)
+add(1,2)
+
