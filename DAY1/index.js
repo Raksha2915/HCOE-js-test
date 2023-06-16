3c95d8ed5943ccfa41a07d5e9674668bd0e88fa8
Raksha Khadka <rakshakhadka58@gmail.com>
2023-06-16 14:01:18 +0545

    Remaining

---
+++ DAY1/index.js
@@ -0,0 +1,22 @@
+//Using var
+var a='1024';
+console.log(a);
+
+//Using let
+let b='123';
+console.log(b);
+
+//Using const
+const c=555;
+console.log(c);
+
+// alert('Hello World')
+
+// const age=prompt('Your age? ');
+// console.log(age)
+
+const isStd=confirm('Are you a student?')
+console.log(isStd)
+
+//operators
+
