7205d270e71a3d64b5ccd6a71c1f7b9a69abaa6e
Raksha Khadka <rakshakhadka58@gmail.com>
2023-06-21 13:57:48 +0545

    modules

--- NODE/app.js
+++ NODE/app.js
@@ -1,6 +1,7 @@
 const http = require('http');
+const currentDateTime = require("./myFirstModule")
 http.createServer((req,res)=>{
     // res.writeHead(200,{'Content_type':'text.html'});
-    res.end('Hello');
+    res.end('Hello World '+currentDateTime.myDateTime());
 }).listen(8080);
 console.log('App running on port 8080');
